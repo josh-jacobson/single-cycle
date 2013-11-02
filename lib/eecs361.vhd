e511ef318368ec9a2175c8278d94846452cb6fcc
Josh Jacobson <joshjacobson14@gmail.com>
2013-11-02 14:23:35 -0500

    Added library files

---
+++ lib/eecs361.vhd
@@ -0,0 +1,139 @@
+-- This package is used for EECS 361 from Northwestern University.
+-- by Kaicheng Zhang (kaichengz@gmail.com)
+
+library ieee;
+use ieee.std_logic_1164.all;
+use work.eecs361_gates.all;
+
+package eecs361 is
+  -- Decoders
+  component dec_n
+    generic (
+      -- Widths of the inputs.
+      n	  : integer
+    );
+    port (
+      src   : in std_logic_vector(n-1 downto 0);
+      z	    : out std_logic_vector((2**n)-1 downto 0)
+    );
+  end component dec_n;
+
+  -- Multiplexors
+  component mux
+    port (
+      sel   : in  std_logic;
+      src0  : in  std_logic;
+      src1  : in  std_logic;
+      z     : out std_logic
+    );
+  end component mux;
+
+  component mux_n
+    generic (
+      -- Widths of the inputs.
+      n	  : integer
+    );
+    port (
+      sel   : in  std_logic;
+      src0  : in  std_logic_vector(n-1 downto 0);
+      src1  : in  std_logic_vector(n-1 downto 0);
+      z     : out std_logic_vector(n-1 downto 0)
+    );
+  end component mux_n;
+
+  component mux_32
+    port (
+      sel   : in  std_logic;
+      src0  : in  std_logic_vector(31 downto 0);
+      src1  : in  std_logic_vector(31 downto 0);
+      z	    : out std_logic_vector(31 downto 0)
+    );
+  end component mux_32;
+
+  -- Flip-flops
+
+  -- D Flip-flops from Figure C.8.4 with a falling edge trigger.
+  component dff
+    port (
+      clk   : in  std_logic;
+      d	    : in  std_logic;
+      q	    : out std_logic
+    );
+  end component dff;
+
+  -- D Flip-flops from Figure C.8.4 with a rising edge trigger.
+  component dffr
+    port (
+      clk   : in  std_logic;
+      d	    : in  std_logic;
+      q	    : out std_logic
+    );
+  end component dffr;
+
+  -- A 32bit SRAM from Figure C.9.1. It can only be used for simulation.
+  component sram
+	generic (
+	  mem_file	: string
+	);
+	port (
+	  -- chip select
+	  cs	: in  std_logic;
+	  -- output enable
+	  oe	: in  std_logic;
+	  -- write enable
+	  we	: in  std_logic;
+	  -- address line
+	  addr	: in  std_logic_vector(31 downto 0);
+	  -- data input
+	  din	: in  std_logic_vector(31 downto 0);
+	  -- data output
+	  dout	: out std_logic_vector(31 downto 0)
+	);
+  end component sram;
+
+  -- Synchronous SRAM with asynchronous reset.
+  component syncram
+    generic (
+	  mem_file	: string
+	);
+	port (
+      -- clock
+      clk   : in  std_logic;
+	  -- chip select
+	  cs	: in  std_logic;
+	  -- write enable
+	  we	: in  std_logic;
+	  -- address line
+	  addr	: in  std_logic_vector(31 downto 0);
+	  -- data input
+	  din	: in  std_logic_vector(31 downto 0);
+	  -- data output
+	  dout	: out std_logic_vector(31 downto 0)
+	);
+  end component syncram;
+
+  -- n-bit full adder.
+  component fulladder_n
+    generic (
+      n : integer
+    );
+    port (
+      cin   : in std_logic;
+      x     : in std_logic_vector(n-1 downto 0);
+      y     : in std_logic_vector(n-1 downto 0);
+      cout  : out std_logic;
+      z     : out std_logic_vector(n-1 downto 0)
+    );
+  end component fulladder_n;
+
+  -- 32-bit full adder.
+  component fulladder_32
+    port (
+      cin   : in std_logic;
+      x     : in std_logic_vector(31 downto 0);
+      y     : in std_logic_vector(31 downto 0);
+      z     : out std_logic_vector(31 downto 0);
+      cout  : out std_logic
+    );
+  end component fulladder_32;
+end;
