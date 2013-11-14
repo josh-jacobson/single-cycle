c4887f3f31da2854a097246d33ebe3092525d04a
Srinivas Balusu <sbb819@selina.ece.northwestern.edu>
2013-11-13 21:16:07 -0600

    sram added

--- or_6.vhd
+++ or_6.vhd
@@ -1,5 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use work.eecs361_gates.all;
+use work.eecs361.all;
 
 entity or_6 is
   port (
@@ -9,6 +11,10 @@ entity or_6 is
 end or_6;
 
 architecture dataflow of or_6 is
+signal xmid : std_logic_vector (2 downto 0);
+signal xlast : std_logic;
 begin
-  z <= x(0) or x(1) or x(2) or x(3) or x(4) or x(5);
+xop1 : or_gate_n generic map (n=>3) port map (x=>x(5 downto 3), y=>x(2 downto 0), z=>xmid);
+xop2 : or_gate port map (x=>xmid(0), y=>xmid(1), z=>xlast);
+xop3 : or_gate port map (x=>xmid(2), y=>xlast, z=>z);
 end dataflow;
