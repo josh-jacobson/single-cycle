2e52a1ba1b8b63483eef778fb6882bc2fe5e01d4
U-gmazin-laptop\gmazin <gmazin@gmazin-laptop.(none)>
2013-11-17 17:15:28 -0600

    register control stuff

--- register_component.vhd
+++ register_component.vhd
@@ -12,6 +12,7 @@ entity register_component is
     busa    : out std_logic_vector(31 downto 0);
     busb    : out std_logic_vector(31 downto 0);
     clk     : in std_logic;
+    reset     : in std_logic;
     we      : in std_logic
   );
 end register_component;
@@ -30,7 +31,7 @@ architecture structural of register_component is
 end component a32bitreg;
 
 subtype regis is std_logic_vector(31 downto 0);
-type register_array is array (0 to 31) of regis;
+type register_array is array (31 downto 0) of regis;
 signal regq : register_array;
 signal regd : register_array;
 
@@ -59,8 +60,8 @@ signal rb_mux5 : std_logic_vector(31 downto 0);
 begin
   REG_GEN: 
    for I in 0 to 31 generate
-      regint_map : a32bitreg port map
-        (q => regq(I), d => regd(I), arst=> '0', clk => clk, aload => "00000000000000000000000000000000", enable => we);
+      regI_map : a32bitreg port map
+        (q => regq(I), d => regd(I), arst=> reset, clk => clk, aload => "00000000000000000000000000000000", enable => we);
    end generate REG_GEN;
    
    allregq <= regq(0) & regq(1) & regq(2) & regq(3) & regq(4) & regq(5) & regq(6) & regq(7) & regq(8) & regq(9) & regq(10) & regq(11) & regq(12) & regq(13) & regq(14) & regq(15) & regq(16) & regq(17) & regq(18) & regq(19) & regq(20) & regq(21) & regq(22) & regq(23) & regq(24) & regq(25) & regq(26) & regq(27) & regq(28) & regq(29) & regq(30) & regq(31);
@@ -80,7 +81,7 @@ begin
    mt4 : mux_n generic map (n=> 64) port map (sel => rs(3), src0 => ra_mux3(127 downto 64), src1=> ra_mux3(63 downto 0), z => ra_mux4);
    mt5 : mux_n generic map (n=> 32) port map (sel => rs(4), src0 => ra_mux4(63 downto 32), src1=> ra_mux4(31 downto 0), z => ra_mux5);
   
-   busb <= ra_mux5;
+   busa <= ra_mux5;
    
    nt1 : mux_n generic map (n=> 512) port map (sel => rt(0), src0 => allregq(1023 downto 512), src1=> allregq(511 downto 0), z => rb_mux1);
    nt2 : mux_n generic map (n=> 256) port map (sel => rt(1), src0 => rb_mux1(511 downto 256), src1=> rb_mux1(255 downto 0), z => rb_mux2);
