a13e9af5eccc880f6f0609fb6993d9c8f1d9f7a6
Josh Jacobson <joshjacobson14@gmail.com>
2013-11-17 16:36:54 -0600

    Control Unit finished, not tested yet

--- control_unit.vhd
+++ control_unit.vhd
@@ -13,9 +13,33 @@ end control_unit;
 
 architecture structural of control_unit is
 
-        -- component declarations here
-begin
+component or_6 is
+  port (
+    x   : in  std_logic_vector(5 downto 0);
+    z   : out std_logic
+  );
+end component or_6;
 
-        -- logic here
+signal not_r_format, r_format, load_store, load, store, not_store : std_logic; 
+
+begin
+-- Set intermediary signals for convenience and readability
+not_r_format_map : or_6 port map (opcode, not_r_format); -- 0 for r-type, 1 otherwise
+r_format_map : not_gate port map (not_r_format, r_format); -- 1 for r-type, 0 otherwise
+load_store_map : and_gate port map(opcode(0), opcode(1), load_store); -- 1 for load or store, 0 otherwise
+store_map : and_gate port map (load_store, opcode(3), store); -- 1 for store instruction, 0 otherwise
+not_store_map : not_gate port map (store, not_store);
+load_map : and_gate port map (load_store, not_store); -- 1 for load instruction, 0 otherwise
+
+-- Assign the final control signals
+RegDst <= r_format;
+ALUSrc <= load_store;
+MemtoReg <= not_r_format;
+RegWrite_map : and_gate port map (r_format, load);
+MemRead <= load;
+MemWrite <= store;
+branch <= opcode(2);
+ALUop(0) <= opcode(2); -- same as branch signal
+ALUop(1) <= r_format;
 
 end architecture structural;
