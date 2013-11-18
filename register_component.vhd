31118a99e79a5505d9efa863e1a531ffaa99717f
U-gmazin-laptop\gmazin <gmazin@gmazin-laptop.(none)>
2013-11-18 16:05:54 -0600

    32 32bit register unit prototype

--- register_component.vhd
+++ register_component.vhd
@@ -6,9 +6,9 @@ use work.eecs361.all;
 entity register_component is
   port (
     busw    : in  std_logic_vector(31 downto 0);
-    rw      : in  std_logic_vector(4 downto 0);
-    ra      : in  std_logic_vector(4 downto 0);
-    rb      : in  std_logic_vector(4 downto 0);
+    rd      : in  std_logic_vector(4 downto 0);
+    rs      : in  std_logic_vector(4 downto 0);
+    rt      : in  std_logic_vector(4 downto 0);
     busa    : out std_logic_vector(31 downto 0);
     busb    : out std_logic_vector(31 downto 0);
     clk     : in std_logic;
@@ -66,11 +66,30 @@ begin
    allregq <= regq(0) & regq(1) & regq(2) & regq(3) & regq(4) & regq(5) & regq(6) & regq(7) & regq(8) & regq(9) & regq(10) & regq(11) & regq(12) & regq(13) & regq(14) & regq(15) & regq(16) & regq(17) & regq(18) & regq(19) & regq(20) & regq(21) & regq(22) & regq(23) & regq(24) & regq(25) & regq(26) & regq(27) & regq(28) & regq(29) & regq(30) & regq(31);
    allregd <= regd(0) & regd(1) & regd(2) & regd(3) & regd(4) & regd(5) & regd(6) & regd(7) & regd(8) & regd(9) & regd(10) & regd(11) & regd(12) & regd(13) & regd(14) & regd(15) & regd(16) & regd(17) & regd(18) & regd(19) & regd(20) & regd(21) & regd(22) & regd(23) & regd(24) & regd(25) & regd(26) & regd(27) & regd(28) & regd(29) & regd(30) & regd(31);
 
-   lt1 : mux_n generic map (n=> 512) port map (sel => rw(0), src0 => allregq(1023 downto 512), src1=> allregq(511 downto 0), z => rw_mux1);
-   lt2 : mux_n generic map (n=> 256) port map (sel => rw(1), src0 => rw_mux1(511 downto 256), src1=> rw_mux1(255 downto 0), z => rw_mux2);
-   lt3 : mux_n generic map (n=> 128) port map (sel => rw(2), src0 => rw_mux1(511 downto 256), src1=> rw_mux1(255 downto 0), z => rw_mux2);
-   lt4 : mux_n generic map (n=> 64) port map (sel => rw(3), src0 => rw_mux1(511 downto 256), src1=> rw_mux1(255 downto 0), z => rw_mux2);
-   lt5 : mux_n generic map (n=> 32) port map (sel => rw(4), src0 => rw_mux1(511 downto 256), src1=> rw_mux1(255 downto 0), z => rw_mux2);
+   lt1 : mux_n generic map (n=> 512) port map (sel => rd(0), src0 => allregd(1023 downto 512), src1=> allregd(511 downto 0), z => rw_mux1);
+   lt2 : mux_n generic map (n=> 256) port map (sel => rd(1), src0 => rw_mux1(511 downto 256), src1=> rw_mux1(255 downto 0), z => rw_mux2);
+   lt3 : mux_n generic map (n=> 128) port map (sel => rd(2), src0 => rw_mux2(255 downto 128), src1=> rw_mux2(127 downto 0), z => rw_mux3);
+   lt4 : mux_n generic map (n=> 64) port map (sel => rd(3), src0 => rw_mux3(127 downto 64), src1=> rw_mux3(63 downto 0), z => rw_mux4);
+   lt5 : mux_n generic map (n=> 32) port map (sel => rd(4), src0 => rw_mux4(63 downto 32), src1=> rw_mux4(31 downto 0), z => rw_mux5);
+  
+   rw_mux5 <= busw;
+   
+   mt1 : mux_n generic map (n=> 512) port map (sel => rs(0), src0 => allregq(1023 downto 512), src1=> allregq(511 downto 0), z => ra_mux1);
+   mt2 : mux_n generic map (n=> 256) port map (sel => rs(1), src0 => ra_mux1(511 downto 256), src1=> ra_mux1(255 downto 0), z => ra_mux2);
+   mt3 : mux_n generic map (n=> 128) port map (sel => rs(2), src0 => ra_mux2(255 downto 128), src1=> ra_mux2(127 downto 0), z => ra_mux3);
+   mt4 : mux_n generic map (n=> 64) port map (sel => rs(3), src0 => ra_mux3(127 downto 64), src1=> ra_mux3(63 downto 0), z => ra_mux4);
+   mt5 : mux_n generic map (n=> 32) port map (sel => rs(4), src0 => ra_mux4(63 downto 32), src1=> ra_mux4(31 downto 0), z => ra_mux5);
+  
+   busb <= ra_mux5;
+   
+   nt1 : mux_n generic map (n=> 512) port map (sel => rt(0), src0 => allregq(1023 downto 512), src1=> allregq(511 downto 0), z => rb_mux1);
+   nt2 : mux_n generic map (n=> 256) port map (sel => rt(1), src0 => rb_mux1(511 downto 256), src1=> rb_mux1(255 downto 0), z => rb_mux2);
+   nt3 : mux_n generic map (n=> 128) port map (sel => rt(2), src0 => rb_mux2(255 downto 128), src1=> rb_mux2(127 downto 0), z => rb_mux3);
+   nt4 : mux_n generic map (n=> 64) port map (sel => rt(3), src0 => rb_mux3(127 downto 64), src1=> rb_mux3(63 downto 0), z => rb_mux4);
+   nt5 : mux_n generic map (n=> 32) port map (sel => rt(4), src0 => rb_mux4(63 downto 32), src1=> rb_mux4(31 downto 0), z => rb_mux5);
+
+   busb <= rb_mux5;
+   
   
 end architecture structural;
 
