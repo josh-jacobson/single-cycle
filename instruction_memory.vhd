fdcaf0c9cfcd2e7a3c8640251bd48ce6deeb5978
Srinivas Balusu <sbb819@selina.ece.northwestern.edu>
2013-11-19 22:50:37 -0600

    added components to processor

--- instruction_memory.vhd
+++ instruction_memory.vhd
@@ -9,13 +9,24 @@ generic (
   );
 port (
     addr : std_logic_vector(31 downto 0);
-    dout :out std_logic_vector(31 downto 0)
+    inst :out std_logic_vector(31 downto 0)
 );
 end instruction_memory;
 architecture structural  of  instruction_memory  is
+   component sram is
+       generic ( mem_file : string );
+       port (
+           cs : in std_logic;
+           oe : in std_logic;
+           we : in std_logic;
+           addr : in std_logic_vector(31 downto 0);
+           din : in std_logic_vector(31 downto 0);
+           dout : out std_logic_vector(31 downto 0)
+        );
+    end component sram;
 begin
    sram_map  :  sram
    generic  map  (mem_file  =>  mem_file)
         port  map  (cs  =>  '1',  oe  =>  '1',  we  =>  '0',  addr  =>  addr,  din  =>  "00000000000000000000000000000000",
-dout  =>  dout);
+dout  =>  inst);
 end architecture;
